18c323ac46b5d2cdee8e98d4688f6ff371e58e20
Sam Stavinoha <smlstvnh@gmail.com>
2014-07-03 00:13:25 -0500

    fill out __init__

--- airbrake/__init__.py
+++ airbrake/__init__.py
@@ -1,1 +1,19 @@
-from handler import AirbrakeHandler
+"""
+    airbrake-python
+    ~~~~~~~~~~~~~~~
+
+    Client for sending python exceptions to airbrake.io
+"""
+
+__version__ = "1.1.0"
+__url__ = "https://github.com/airbrake/airbrake-python"
+__notifier__ = {
+    'name': 'airbrake-python',
+    'version': __version__,
+    'url': __url__
+}
+
+
+from airbrake import utils
+from airbrake.notifier import Airbrake
+from airbrake.handler import AirbrakeHandler
