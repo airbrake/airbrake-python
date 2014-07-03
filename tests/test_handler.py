9a572b38997cc4fe9f0a7956a02f4cf04922d02d
Sam Stavinoha <smlstvnh@gmail.com>
2014-07-03 00:27:08 -0500

    add a few unittests

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,40 +1,65 @@
 import logging
-import os
+import unittest
+
+from testfixtures import log_capture
 
 import airbrake
 
-PROJECT_ID = os.environ['AIRBRAKE_PROJECT_ID']
-API_KEY = os.environ['AIRBRAKE_API_KEY']
 
 
-LOG = logging.getLogger(__name__)
-ab = airbrake.AirbrakeHandler(PROJECT_ID, API_KEY, "sam-test-handler")
-#ab.airbrake.deploy()
-ab.setLevel(logging.DEBUG)
-LOG.addHandler(ab)
-LOG.setLevel(logging.DEBUG)
+class TestAirbrakeLoggerHelper(unittest.TestCase):
 
-def foo():
-    log_exception()
-    log_debug()
+    def test_auto_logger_name_is_calling_module(self):
+        logger = airbrake.getLogger()
+        self.assertEqual(logger.name, airbrake.DEFAULT_LOGGER_PREFIX + __name__)
 
+    def test_auto_logger_has_airbrake_handler(self):
+        logger = airbrake.getLogger()
+        isabhandler = lambda x: isinstance(x, airbrake.AirbrakeHandler)
+        self.assertTrue(any(map(isabhandler, logger.handlers)))
 
-def log_exception():
-    try:
-        d = {}
-        d['no']
-    except Exception as e:
-        LOG.exception(e)
+    def test_auto_logger_has_level(self):
+        logger = airbrake.getLogger()
+        self.assertTrue(
+            logger.isEnabledFor(airbrake.handler.DEFAULT_LOGGING_LEVEL))
 
+    def test_auto_logger_given_name(self):
+        logger = airbrake.getLogger('my_module')
+        self.assertTrue(
+            logger.isEnabledFor(airbrake.handler.DEFAULT_LOGGING_LEVEL))
 
-def log_debug():
-    LOG.debug("I am a DEBUG logging message.")
+class TestAirbrakeHandler(unittest.TestCase):
 
-def main():
-    foo()
+    def setUp(self):
+        self.logger = airbrake.getLogger()
+        self.logmsg = "There's your problem, right there."
 
+    @log_capture(level=airbrake.handler.DEFAULT_LOGGING_LEVEL)
+    def do_some_logs(self, l):
+        self.logger.info("Should ignore this by default.")
+        self.logger.error(self.logmsg)
+        return l
+
+    def log_in_exception_handler(self):
+        try:
+            1/0
+        except Exception:
+            self.logger.exception("Hate when this happens.", extra={'this': 'wins'})
+
+        try:
+            undefined
+        except Exception:
+            self.logger.error("It's bad luck not to assign values.")
+
+    def test_log_captures(self):
+        captured = self.do_some_logs()
+        captured.check(
+            (airbrake.DEFAULT_LOGGER_PREFIX + __name__,
+             logging._levelNames[airbrake.handler.DEFAULT_LOGGING_LEVEL],
+             self.logmsg))
+
+    def test_exception(self):
+        self.log_in_exception_handler()
 
 if __name__ == '__main__':
-
-    # multiple fn calls to generate traceback
-    main()
+    unittest.main()
