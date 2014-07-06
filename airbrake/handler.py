976d72848dad98fcbbc44906671f050a33d0f692
Sam Stavinoha <smlstvnh@gmail.com>
2014-07-06 16:30:37 -0500

    add airbrake_error_from_logrecord
    includes style/docstring updates

--- airbrake/handler.py
+++ airbrake/handler.py
@@ -1,3 +1,12 @@
+"""AirbrakeHandler module.
+
+All functions and types related to python logging
+should be defined in this module.
+
+A function for mapping a LogRecord object
+    https://docs.python.org/2/library/logging.html#logrecord-objects
+to an Airbrake error should be included here.
+"""
 import logging
 import sys
 
@@ -15,45 +24,65 @@ class AirbrakeHandler(logging.Handler):
         * an instance of airbrake.Airbrake
     """
     def __init__(self, airbrake=None, level=DEFAULT_LOGGING_LEVEL, **kwargs):
-        """Initialize the Airbrake handler with a default loggin level.
+        """Initialize the Airbrake handler with a default logging level.
 
         Default level of logs handled by this class are >= ERROR,
         which includes ERROR and CRITICAL. To change this behavior
         supply a different arguement for 'level'.
+
+        All 'kwargs' will be passed to notifier.Airbrake to instantiate
+        a notifier client.
         """
 
         logging.Handler.__init__(self, level=level)
 
         if isinstance(airbrake, Airbrake):
-            airbrake.auto_notify = True
             self.airbrake = airbrake
         else:
-            kwargs.pop('auto_notify', '')
-            self.airbrake = Airbrake(auto_notify=True, **kwargs)
+            self.airbrake = Airbrake(**kwargs)
 
     def emit(self, record):
         try:
-            params = {
-                'created': record.created,
-                'process_id': record.process,
-                'process_name': record.processName,
-                'thread_name': record.threadName,
-                'lineno': record.lineno,
-                'pathname': record.pathname,
-                'funcName': record.funcName,
-                'msg': record.getMessage()}
-
-            # find params from kwarg 'extra'
-            for key, val in vars(record).items():
-                if not hasattr(FAKE_LOGRECORD, key):
-                    params[key] = val
-
-            errtype = "%s:%s" % (record.levelname, record.filename)
-            self.airbrake.log(
-                exc_info=record.exc_info, message=record.getMessage(),
-                filename=record.pathname, line=record.lineno,
-                function=record.funcName, errtype=errtype, **params)
+            airbrakeerror = airbrake_error_from_logrecord(record)
+            self.airbrake.log(**airbrakeerror)
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
             self.handleError(record)
+
+def airbrake_error_from_logrecord(record):
+    """Create an airbrake error dictionary from a python LogRecord object.
+
+    For more info on the logging.LogRecord class:
+        https://docs.python.org/2/library/logging.html#logrecord-objects
+    """
+    airbrakeerror = {}
+    params = {
+        'created': record.created,  # TODO(samstav): make this human readable
+        'process_id': record.process,
+        'process_name': record.processName,
+        'thread_name': record.threadName,
+        'lineno': record.lineno,
+        'pathname': record.pathname,
+        'funcName': record.funcName,
+        'msg': record.getMessage()}
+
+    # find params from kwarg 'extra'
+    for key, val in vars(record).items():
+        if not hasattr(FAKE_LOGRECORD, key):
+            params[key] = val
+
+    if sys.exc_info()[1]:
+        airbrakeerror['errtype'] = sys.exc_info()[1].__class__.__name__
+    else:
+        # set the errtype even if there's no current exception stackframe
+        airbrakeerror['errtype'] = "%s:%s" % (record.levelname,
+                                              record.filename)
+
+    airbrakeerror['params'] = params
+    airbrakeerror['exc_info'] = record.exc_info
+    airbrakeerror['message'] = record.getMessage()
+    airbrakeerror['filename'] = record.pathname
+    airbrakeerror['line'] = record.lineno
+    airbrakeerror['function'] = record.funcName
+    return airbrakeerror
