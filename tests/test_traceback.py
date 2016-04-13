6a5b9b9677b3e3cac518d1845de6c6350d909a57
Patrick Hayes <pfhayes@gmail.com>
2016-04-13 09:35:22 -0700

    Fix tests, lint

--- tests/test_traceback.py
+++ tests/test_traceback.py
@@ -1,6 +1,4 @@
-import mock
 import sys
-import traceback
 import unittest
 
 from airbrake.notifier import format_backtrace
@@ -17,8 +15,8 @@ class FakeTraceback(object):
 class TestTraceback(unittest.TestCase):
     def _exc_info(self):
         try:
-            1/0
-        except ZeroDivisionError as e:
+            1 / 0
+        except ZeroDivisionError:
             return sys.exc_info()
 
     def _make_fake_traceback(self, tb):
@@ -27,13 +25,13 @@ class TestTraceback(unittest.TestCase):
     def test_format(self):
         tb = self._exc_info()[2]
         fake_tb = self._make_fake_traceback(tb)
-        assert format_backtrace(tb) == format_backtrace(fake_tb)
+        self.assertEqual(format_backtrace(tb), format_backtrace(fake_tb))
 
     def test_is_exc_info_tuple(self):
         exc_info = self._exc_info()
-        assert is_exc_info_tuple(exc_info)
-        exc_info = exc_info[0], exc_info[1], self._make_fake_traceback(exc_info[2])
-        assert is_exc_info_tuple(exc_info)
+        self.assertTrue(is_exc_info_tuple(exc_info))
+        fake_tb = self._make_fake_traceback(exc_info[2])
+        self.assertTrue(is_exc_info_tuple((exc_info[0], exc_info[1], fake_tb)))
 
 
 if __name__ == '__main__':
