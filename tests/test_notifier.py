airbrake/airbrake-python

commit 1d7b191a2cc3b53f76f12d09f68c857183859e08

Don't fail when 'extra' in log records contain non JSON serializable objects

airbrake/notifier.py
@@ -186,15 +186,13 @@ class Airbrake(object):
                    'notifier': self.notifier,
                    'environment': environment,
                    'session': session}
-
-        return self.notify(payload)
+        return self.notify(json.dumps(payload, cls=utils.FailProofJSONEncoder))
 
     def notify(self, payload):
         """Post the current errors payload body to airbrake.io."""
         headers = {'Content-Type': 'application/json'}
         api_key = {'key': self.api_key}
-
-        response = requests.post(self.api_url, data=json.dumps(payload),
+        response = requests.post(self.api_url, data=payload,
                                  headers=headers, params=api_key)
         response.raise_for_status()
         return response

tests/test_notifier.py
@@ -1,16 +1,21 @@
 import airbrake
 import mock
 import unittest
+import json
+
 from airbrake.notifier import Airbrake
 
 
 class TestAirbrakeNotifier(unittest.TestCase):
 
-    def _create_notify(test, exception, session={}, environment={}):
+    def _create_notify(test, exception, session={}, environment={}, **params):
         def notify(self, payload):
+            payload = json.loads(payload)
             test.assertEqual(session, payload['session'])
             test.assertEqual(environment, payload['environment'])
             test.assertEqual(str(exception), payload['errors'][0]['message'])
+            for param_name, expected_value in params.items():
+                test.assertEqual(expected_value, payload['params'][param_name])
         return notify
 
     def setUp(self):
@@ -45,6 +50,21 @@ class TestAirbrakeNotifier(unittest.TestCase):
         notify = self._create_notify(msg, environment=self.environment)
         with mock.patch.object(Airbrake, 'notify', notify):
             extra = {'environment': self.environment}
+            self.logger.exception(Exception(msg), extra=extra)
+
+    def test_exception_with_non_serializable(self):
+        msg = "Narf!"
+
+        class NonSerializable:
+            def __repr__(self):
+                return '<Use this instead>'
+        non_serializable = NonSerializable()
+
+        notify = self._create_notify(msg,
+                                     very='important',
+                                     jsonify=repr(non_serializable))
+        with mock.patch.object(Airbrake, 'notify', notify, 'jsonify'):
+            extra = {'very': 'important', 'jsonify': non_serializable}
             self.logger.exception(Exception(msg), extra=extra)
 
 if __name__ == '__main__':

airbrake/__about__.py
@@ -16,7 +16,7 @@ __title__ = 'airbrake'
 __summary__ = 'Python SDK for airbrake.io'
 __author__ = 'BK Box, Sam Stavinoha'
 __email__ = 'samuel.stavinoha@rackspace.com'
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 __keywords__ = ['airbrake', 'exceptions', 'airbrake.io']
 __license__ = 'Apache License, Version 2.0'
 __url__ = 'https://github.com/airbrake/airbrake-python'

airbrake/utils.py
@@ -8,12 +8,22 @@ except ImportError:
 
 import traceback
 import types
+import json
 
 try:
     TypeType = types.TypeType
 except AttributeError:
     # For >= Python 3
     TypeType = type
+
+
+class FailProofJSONEncoder(json.JSONEncoder):
+    """Uses object's representation for unsupported types."""
+
+    # pylint: disable=E0202
+    def default(self, o):
+        """Return object's repr when not JSON serializable."""
+        return repr(o)
 
 
 class CheckableQueue(Queue):

commit fbc78dc6204736f2939aa2c7cde14e12ade4c337

Fix `Airbrake.notify` regression
1d7b191a2cc3b53f76f12d09f68c857183859e08 broke the `Airbrake.notify`
interface by assuming that the `payload` argument is already encoded as
a JSON string. Unfortunately, the tests didn't catch this because the
test weren't actually exercising the `notify` method. :sad_panda:

This patch fixes the regression introduced in
https://github.com/airbrake/airbrake-python/pull/27 and adds a test to
notify to ensure that this doesn't happen again.

airbrake/notifier.py
@@ -186,14 +186,24 @@ class Airbrake(object):
                    'notifier': self.notifier,
                    'environment': environment,
                    'session': session}
-        return self.notify(json.dumps(payload, cls=utils.FailProofJSONEncoder))
+        return self.notify(payload)
 
     def notify(self, payload):
-        """Post the current errors payload body to airbrake.io."""
+        """Post the current errors payload body to airbrake.io.
+
+        :param dict payload:
+            Notification payload, in a dict/object form. The notification
+            payload will ultimately be sent as a JSON-encoded string, but here
+            it still needs to be in object form.
+        """
         headers = {'Content-Type': 'application/json'}
         api_key = {'key': self.api_key}
-        response = requests.post(self.api_url, data=payload,
-                                 headers=headers, params=api_key)
+        response = requests.post(
+            self.api_url,
+            data=json.dumps(payload, cls=utils.FailProofJSONEncoder,
+                            sort_keys=True),
+            headers=headers,
+            params=api_key)
         response.raise_for_status()
         return response
 

tests/test_notifier.py
@@ -1,7 +1,6 @@
 import airbrake
 import mock
 import unittest
-import json
 
 from airbrake.notifier import Airbrake
 
@@ -10,7 +9,6 @@ class TestAirbrakeNotifier(unittest.TestCase):
 
     def _create_notify(test, exception, session={}, environment={}, **params):
         def notify(self, payload):
-            payload = json.loads(payload)
             test.assertEqual(session, payload['session'])
             test.assertEqual(environment, payload['environment'])
             test.assertEqual(str(exception), payload['errors'][0]['message'])
@@ -66,6 +64,25 @@ class TestAirbrakeNotifier(unittest.TestCase):
         with mock.patch.object(Airbrake, 'notify', notify, 'jsonify'):
             extra = {'very': 'important', 'jsonify': non_serializable}
             self.logger.exception(Exception(msg), extra=extra)
+
+    def test_notify_payload(self):
+        # 1d7b191a2cc3b53f76f12d09f68c857183859e08 broke the `notify interface
+        # by making the assumption that the payload sent to `notify` is already
+        # encoded as a JSON string.
+        # This test ensures that such a regression can't happen again.
+        with mock.patch('requests.post') as requests_post:
+            ab = Airbrake(project_id=1234, api_key='fake', environment='test')
+            payload = dict(foo=1, bar=2)
+            ab.notify(payload)
+
+            expected_call_args = mock.call(
+                'https://airbrake.io/api/v3/projects/1234/notices',
+                data='{"bar": 2, "foo": 1}',
+                headers={'Content-Type': 'application/json'},
+                params={'key': 'fake'}
+            )
+            self.assertEqual(expected_call_args, requests_post.call_args)
+
 
 if __name__ == '__main__':
     unittest.main()